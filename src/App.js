4552e0bc1c2144ef0fd93ff06672129e75794732
burgic <ceburgin@gmail.com>
2022-04-05 09:11:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [items, setItems] = useState([
+    {name: "Take Dog Out", highPriority: true},
+    {name: "Clean Fridge", highPriority: false},
+    {name: "Walk", highPriority: true}
+  ]);
+
+  const[newItem, setNewItem] = useState('');
+
+  const handleItemInput = (event) => {
+    setNewItem(event.target.value);
+  }
+
+  const saveNewItem = (event) => {
+    event.preventDefault();
+    const copyItems = [... items];
+    copyItems.push({name: newItem});
+    setItems(copyItems);
+    setNewItem('');
+  }
+
+  const addItem = (index) => {
+    const copyItems = [...items];
+    copyItems[index].highPriority = true;
+    setItems(copyItems);
+  }
+
+  const itemNodes = items.map((item, index) => {
+    return(
+      <li key={index} className={item.highPriority ? 'high-priority' : 'low-priority' }>
+        <span>{item.name}</span>
+        {/* {item.highPriority ? <span className = 'high-priority'>High Priority</span> :
+        <button onClick={ () => addItem(index)}>Banana</button>} */}
+      </li>
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="toDoList">
+
+      <h1>To Do List</h1>
+      <hr></hr>
+
+      <ul>
+        {itemNodes}
+      </ul>
+
+      <form onSubmit={saveNewItem}>
+        <label htmlFor='new-item'>Add new item</label>
+        <input id="new-item" type='text' value={newItem} onChange={handleItemInput}/>
+        <input type='submit' value='Save New Item'/>
+        
+      </form>
+
     </div>
+
   );
 }
 
